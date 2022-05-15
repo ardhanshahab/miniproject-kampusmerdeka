e715ad7555180309e5dcee1e3b77b43517f17a6b
ardhanshahab <ardhanshahab@gmail.com>
2022-05-15 11:00:00 +0700

    feat : hasuraerror2

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    apollo: {
+      enableMocks: false,
+      enableEngine: false
+    }
+  }
 })
